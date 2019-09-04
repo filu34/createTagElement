6a11adb83bf175a25801ef248c18167802d64434
Filip Szettel <filu3@protonmail.com>
2019-09-04 23:12:41 +0100

    first commit
    First example of simple code, easy to understand by beginners in JavaScript.

---
+++ createTagElement.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+const attributes = [["class", "test"], ["style", "background-color: red"]];
+
+const createTagEl = function(parent, child, atrArrayObj) {
+  child = document.createElement(child);
+  parent.appendChild(child).setAttribute();
+
+  for (let i = 0; i < atrArrayObj.length; i++) {
+    child.setAttribute(atrArrayObj[i][0], atrArrayObj[i][1]);
+  };
+  return child;
+};
+
+createTagEl(body, "div", attributes);
