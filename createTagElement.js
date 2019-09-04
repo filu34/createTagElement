af911a62600a520e4023713090f0e07d0bcf2023
Filip Szettel <filu3@protonmail.com>
2019-09-05 00:01:40 +0100

    delete .setAttribute in 6 line

--- createTagElement.js
+++ createTagElement.js
@@ -3,7 +3,7 @@ const attributes = [["class", "test"], ["style", "background-color: red"]];
 
 const createTagEl = function(parent, child, atrArrayObj) {
   child = document.createElement(child);
-  parent.appendChild(child).setAttribute();
+  parent.appendChild(child);
 
   for (let i = 0; i < atrArrayObj.length; i++) {
     child.setAttribute(atrArrayObj[i][0], atrArrayObj[i][1]);
